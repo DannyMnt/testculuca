3ffa9f89ea0000a91ea0ef46904069774f68df60
Mouawaz <99729264+Mouawaz@users.noreply.github.com>
2024-11-08 21:47:51 +0100

    gitignore debug

--- Doctor/Doctor/obj/Debug/net8.0/Doctor.AssemblyInfo.cs
+++ Doctor/Doctor/obj/Debug/net8.0/Doctor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Doctor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c7b41e7a274fbfa15279ce059449b75001d905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6163c052b934950f115456a240778cc7ae7c01c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Doctor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Doctor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PatientFinal/PatientFinal/obj/Debug/net8.0/PatientFinal.AssemblyInfo.cs
+++ PatientFinal/PatientFinal/obj/Debug/net8.0/PatientFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7760dec478ed635cfd0ea11b8115c2d6ce5a07b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6163c052b934950f115456a240778cc7ae7c01c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
